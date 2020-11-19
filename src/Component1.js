20634f3f58f610cf4e1f5d8127d9057e929f74e2
nosamendev <nosamendev@gmail.com>
2020-11-19 14:48:29 +0200

    currency input added

--- src/Component1.js
+++ src/Component1.js
@@ -1,21 +1,33 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import CurrencyInput from 'react-currency-input-field';
 import {changeText1, changeText2, changeText3}  from './store/actions';
 
 const Component1 = (props) => {
 
     useEffect(() => {
         props.changeText1(props.text);
-      }, []);
+      }, []); 
 
     const handleComponentClick = () => {
         props.changeText1('Component1');
         props.changeText2('Hello');
         props.changeText3('Hello');
-    }
+    }  
 
     return (
-        <div className="component" onClick={handleComponentClick}>{props.text1}</div>
+        <div className="component" onClick={handleComponentClick}>
+            <p>{props.text1}</p>
+            <CurrencyInput
+                id="input-example"
+                name="currencyInput"
+                placeholder="$1,000"
+                defaultValue={1000}
+                allowDecimals={true}
+                decimalsLimit={2}
+                prefix="$"
+            />
+        </div>
     );
 }
 const mapStateToProps = (state) => {
